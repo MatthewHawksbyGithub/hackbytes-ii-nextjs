52cb0f015036800024f7100f45a3d10bbb5c08bc
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-30 22:51:07 -0700

    feat: add Stage to Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,17 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { api } from '~/utils/api';
+import dynamic from 'next/dynamic';
+import { GameWrapper } from '~/components/game/GameWrapper';
+import { Stage } from '@pixi/react';
+import { Game } from '~/components/game/Game';
+
+// const GameWrapper = dynamic(
+// 	() => import('~/components/game/GameWrapper').then((mod) => mod.GameWrapper),
+// 	{
+// 		ssr: false,
+// 	}
+// );
 
 export default function Home() {
 	const hello = api.example.hello.useQuery({ text: 'from tRPC' });
@@ -21,42 +32,15 @@ export default function Home() {
 					href='/favicon/favicon.svg'
 				/>
 			</Head>
+
 			<main className={styles.main}>
-				<div className={styles.container}>
-					<h1 className={styles.title}>
-						Create <span className={styles.pinkSpan}>T3</span> App
-					</h1>
-					<div className={styles.cardRow}>
-						<Link
-							className={styles.card}
-							href='https://create.t3.gg/en/usage/first-steps'
-							target='_blank'
-						>
-							<h3 className={styles.cardTitle}>First Steps →</h3>
-							<div className={styles.cardText}>
-								Just the basics - Everything you need to know to set up your
-								database and authentication.
-							</div>
-						</Link>
-						<Link
-							className={styles.card}
-							href='https://create.t3.gg/en/introduction'
-							target='_blank'
-						>
-							<h3 className={styles.cardTitle}>Documentation →</h3>
-							<div className={styles.cardText}>
-								Learn more about Create T3 App, the libraries it uses, and how
-								to deploy it.
-							</div>
-						</Link>
-					</div>
-					<div className={styles.showcaseContainer}>
-						<p className={styles.showcaseText}>
-							{hello.data ? hello.data.greeting : 'Loading tRPC query...'}
-						</p>
-						<AuthShowcase />
-					</div>
-				</div>
+				<Stage
+					width={600}
+					height={400}
+					options={{ backgroundColor: 0x999999 }}
+				>
+					<Game />
+				</Stage>
 			</main>
 		</>
 	);
