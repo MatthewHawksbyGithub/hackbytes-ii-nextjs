1bf250b72bee0d1f8e12995d54de672592b252c3
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-30 22:34:49 -0700

    feat: move Game component into game/ folder

--- src/components/Game.tsx
+++
@@ -1,9 +0,0 @@
-// component to wrap the game
-
-export default function Game() {
-	return (
-		<>
-			<div></div>
-		</>
-	);
-}
---
+++ src/components/game/Game.tsx
@@ -0,0 +1,13 @@
+import { Item } from './game-components/Item';
+
+export function Game() {
+	return (
+		<>
+			<Item
+				itemNum='001'
+				x={150}
+				y={150}
+			/>
+		</>
+	);
+}
