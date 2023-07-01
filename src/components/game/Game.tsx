c606c1a39a87345f53d844299d70bed181465bac
Ryan Milligan <ryanmilligan88@gmail.com>
2023-07-01 14:17:16 -0700

    feat: add Bins to Game

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,12 +1,33 @@
+import { Bin } from './game-components/Bin';
 import { Item } from './game-components/Item';
 
 export function Game() {
 	return (
 		<>
+			<Bin
+				color={0xff0000}
+				x={300}
+				y={675}
+			/>
+			<Bin
+				color={0x00ff00}
+				x={500}
+				y={675}
+			/>
+			<Bin
+				color={0x7d8cc4}
+				x={700}
+				y={675}
+			/>
+			<Bin
+				color={0xffffff}
+				x={900}
+				y={675}
+			/>
 			<Item
-				itemNum='001'
-				x={150}
-				y={150}
+				itemNum='066'
+				x={500}
+				y={400}
 			/>
 		</>
 	);
